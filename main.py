483fde7ab17131f286660cac89345a4a0513b7ad
cubanezu9999 <71436676+cubanezu9999@users.noreply.github.com>
2021-09-27 21:34:08 +0100

    second attempt

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
-from flask import Flask, render_template, request
+from flask import Flask,redirect, render_template, request
 
 app = Flask(__name__)
+
+peoples = []
+
 
 @app.route("/")
 def home():
     name = request.args.get("name","World")
     return render_template("index.html", name = name)
+
+@app.route("/Inreg")
+def reg():
+    return render_template("inreg.html", peoples=peoples)
+
 @app.route("/Favorites", methods = ["POST"])
 def Favorites():
     firstname = request.form.get("firstname")
     lastname = request.form.get("lastname")
-    favoritegame = request.form.get("game")
+    game = request.form.get("game")
     if not firstname or not lastname :
-        return "Naspa!Baga ma dracu ce trebe acolo si apoi apasa submit"
-    else:
-       return render_template("succes.html")
+        return render_template("failure.html")
+    
+       
+    peoples.append(f"{firstname} {lastname} ii place sa joace {game}")
+    return redirect("/Inreg")
     
 
 
